276f4a990c0b83723ede74b90bf37d5fb3646d1b
harry <haesoo9410@gmail.com>
2020-10-07 21:24:11 +0900

    image download 5

--- Naver_Image_Downloader.py
+++ Naver_Image_Downloader.py
@@ -8,12 +8,15 @@ res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
 
 
-# with open("images.html", "w", encoding="utf8") as f :
-#     f.write(soup.prettify()) # html문서를 예쁘게
-
 images = soup.find_all("img", attrs={"class":"_img"})
-# print(images)
-
 
 for idx, image in enumerate(images):
-       print(image["data-source"])
+       image_url = image["data-source"]
+       image_res = requests.get(image_url)
+       image_res.raise_for_status()
+       with open("image_{}_{}.jpg".format(x, idx + 1), "wb") as f: # 텍스트가 아니므로 wb 텍스트는 w
+              f.write(image_res.content) # .content = 내용을 씀
+
+       if idx >= 4:  # 상위 5개 이미지까지만 다운로드
+              break
+print("저장 완료")
